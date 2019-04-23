9c8edb08750711015bd3f6f00e33e581e4158d1d
Maxime <maxime.tajan@outlook.com>
2019-04-23 10:15:14 +0200

    Essai de machine à etat pour gestion du menu, pas encore fonctionnel

--- src/ButtonsGPS.cpp
+++ src/ButtonsGPS.cpp
@@ -7,8 +7,6 @@
 #define pinBtnEn 17
 
 
-//class ButtonsGPS {
-
   ButtonsGPS::ButtonsGPS()
   {
     debouncer = Bounce();
@@ -22,31 +20,66 @@
 
 
   ButtonsGPS::readButtons(){
-    debouncer.update();
+    int state = 0;
 
-    if (debouncer.read()) {
-      switch (digitalRead(pinBtn1) * 2 + digitalRead(pinBtn0)) {
-        case 0:
-          return 1;
-          //break;
+    while(state == 0){
+      debouncer.update();
+      if (debouncer.read()) {
+        switch (digitalRead(pinBtn1) * 2 + digitalRead(pinBtn0)) {
+          case 0:
+            state = 1;
+            break;
 
-        case 1:
-          return 2;
-        //  break;
+          case 1:
+            state = 2;
+            break;
 
-        case 2:
-          return 3;
-        //  break;
+          case 2:
+            state = 3;
+            break;
 
-        case 3:
-          return 4;
-        //  break;
+          case 3:
+            state = 4;
+            break;
 
+        }
       }
-    } else{
-      //lcd.setCursor(0, 0);
-      return 0;
+
+
+
+
+
+
+      // while(state == 0){
+      //   debouncer.update();
+      //   if (debouncer.read()) {
+      //     switch (digitalRead(pinBtn1) * 2 + digitalRead(pinBtn0)) {
+      //       case 0:
+      //         return 1;
+      //         //break;
+      //
+      //       case 1:
+      //         return 2;
+      //       //  break;
+      //
+      //       case 2:
+      //         return 3;
+      //       //  break;
+      //
+      //       case 3:
+      //         return 4;
+      //       //  break;
+      //
+      //     }
+      //   }
+      // else{
+      //   //lcd.setCursor(0, 0);
+      //   return 0;
+      // }
+
     }
+
+    return state;
   }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,53 @@
 #include <SD.h>
 
 
+//Menu
+const int SYSTEM = 0, GPS = 1, STATS = 2;
+
+const int BATTERY = 4, SDCARD = 5, USB = 3;
+
+const int SDSIZE = 6, FORMAT = 7;
+
+const int PARCOURS = 8, RECORD = 9;
+
+const int TOTAL = 10, PARCOURS_CHOICE = 11;
+
+//Tableau dont l'index est le currentState et l'état vers lequel mène chacun des boutons (1,2,3,4)
+const int state[][4]={
+  //SYSTEM ok
+  {SYSTEM, STATS, GPS, BATTERY},
+  //GPS ok
+  {GPS, SYSTEM, STATS, PARCOURS},
+  //STATS ok
+  {STATS, GPS, SYSTEM, TOTAL},
+
+
+  //BATTERY
+  {SYSTEM, USB, SDCARD, BATTERY},
+  //SDCARD
+  {SYSTEM, BATTERY, USB, SDSIZE},
+  //USB
+  {SYSTEM, SDCARD, BATTERY, USB},
+
+
+  //SDSIZE
+  {SDCARD, FORMAT, FORMAT, SDSIZE},
+  //FORMAT
+  {SDCARD, SDSIZE, SDSIZE, FORMAT},
+
+
+  //PARCOURS
+  {GPS, RECORD, RECORD, PARCOURS},
+  //RECORD
+  {GPS, PARCOURS, PARCOURS, RECORD},
+
+
+  //TOTAL
+  {STATS, PARCOURS_CHOICE, PARCOURS_CHOICE, TOTAL},
+  //PARCOURS_CHOICE
+  {STATS, TOTAL, TOTAL, PARCOURS_CHOICE}
+};
+
 
 //Carte SD
 #define pinSD A0
@@ -22,19 +69,19 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 ButtonsGPS btn;
 
-
+int currentState;
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(pinBat, INPUT);
 
-
   lcd.begin(8, 2);
-  //lcd.noDisplay();
-  //lcd.display();
 
+  currentState = SYSTEM;
 }
+
+
 
 
 
@@ -42,18 +89,77 @@ void setup() {
 void loop() {
 
 
+  //AFFICHAGE TENSION
   //Prend en compte la différence entre le 3.3V d'Arduino et le 3.25V Max de la batterie
   //tension = (float(analogRead(pinBat))/1007.0)*6.5;
-  tension = (float)(analogRead(pinBat))/1023.0*6.5;
-  lcd.setCursor(0, 1);
-  lcd.print(tension);
+  // tension = (float)(analogRead(pinBat))/1023.0*6.5;
+  // lcd.setCursor(0, 1);
+  // lcd.print(tension);
+  //
+  //
+  //
+  // //AFFICHAGE VALEUR BOUTON
+  // lcd.setCursor(0, 0);
+  // lcd.print(btn.readButtons());
 
 
 
-
+  //Print current state
+  lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print(btn.readButtons());
+  switch (currentState) {
+    case SYSTEM:
+      lcd.print("System");
+      break;
+    case GPS:
+      lcd.print("GPS");
+      break;
+    case STATS:
+      lcd.print("Stats");
+      break;
 
+
+    case BATTERY:
+      lcd.print("Battery");
+      break;
+    case SDCARD:
+      lcd.print("SD card");
+      break;
+    case USB:
+      lcd.print("USB");
+      break;
+
+
+    case SDSIZE:
+      lcd.print("Size");
+      break;
+    case FORMAT:
+      lcd.print("Format");
+      break;
+
+
+    case PARCOURS:
+      lcd.print("Parcours");
+      break;
+    case RECORD:
+      lcd.print("Record");
+      break;
+
+
+    case TOTAL:
+      lcd.print("Total");
+      break;
+    case PARCOURS_CHOICE:
+      lcd.print("Parcours");
+      break;
+    }
+
+    int button = (btn.readButtons()-1);
+    //int etat = currentState;
+    lcd.setCursor(0, 1);
+    lcd.print(button);
+
+    currentState = state[currentState][button];
 
 
 }
